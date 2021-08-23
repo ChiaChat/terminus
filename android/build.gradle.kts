75c488688236b11e678f3931c051372177cb0669
Andrew Bueide <andrew@abueide.com>
2021-08-22 20:38:16 -0500

    Working android build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,23 +9,25 @@ group = "org.chiachat"
 version = "1.0.0"
 
 dependencies {
-    api(compose.desktop.currentOs)
-    api(compose.runtime)
-    api(compose.foundation)
-    api(compose.material)
-    api(compose.ui)
-    api(compose.uiTooling)
-    api(compose.preview)
-    api("androidx.appcompat:appcompat:1.3.1")
-    api("androidx.core:core-ktx:1.6.0")
-    api("androidx.activity:activity-compose:1.3.1")
+    implementation(project(":core"))
+    implementation(compose.desktop.currentOs)
+    implementation(compose.runtime)
+    implementation(compose.foundation)
+    implementation(compose.material)
+    implementation(compose.ui)
+    implementation(compose.uiTooling)
+    implementation(compose.preview)
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
 
 }
 
 android {
     compileSdkVersion(31)
     defaultConfig {
-        applicationId = "org.chiachat.kchia.android"
+        applicationId = "org.chiachat.terminus.android"
         minSdkVersion(24)
         targetSdkVersion(31)
         versionCode = 1
--- android/src/main/java/org/chiachat/terminus/android/MainActivity.kt
+++ android/src/main/java/org/chiachat/terminus/android/MainActivity.kt
@@ -1,13 +1,16 @@
+package org.chiachat.terminus.android;
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import org.chiachat.terminus.Platform
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello Android!")
+            Text("Hello ${Platform.getPlatform()}")
         }
     }
 }
